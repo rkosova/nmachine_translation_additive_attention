ac3bd8d41ba018c8f07eb5f6c5653155f048224c
Ron Kosova <ron.kosova@gmail.com>
2024-01-18 17:41:47 +0100

    Docstrings for encoder

--- bahdanaunmt/models/encoder.py
+++ bahdanaunmt/models/encoder.py
@@ -1,7 +1,15 @@
 from torch import nn
 
 class Encoder(nn.Module):
+    """Simple RNN encoder with learned embedding and GRU cells
+    """
     def __init__(self, input_vocab_len, hidden_size, dropout_p=0.1) -> None:
+        """
+        Args:
+            input_vocab_len (int): Length of the input language vocabulary
+            hidden_size (int): Size of the encoder hidden states
+            drouput_p (float) = 0.1: Dropout percentage for embedding layer
+        """
         super(Encoder, self).__init__()
         self.hidden_size = hidden_size
         
@@ -10,6 +18,10 @@ class Encoder(nn.Module):
         self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, x):
+        """
+        Args:
+            x (torch.Tensor): Input sentence
+        """
         embedded = self.dropout(self.embedding(x))
         output, hidden = self.gru(embedded)
 
